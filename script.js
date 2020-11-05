ea89194e4e6fbc271c37c24cdb99f182cb01039e
vimal Raveendran <vimalraveendra@gmail.com>
2020-11-05 13:18:48 +0100

    updated script & html files to render the weather

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-const API_KEY= process.env
-console.log("api",API_KEY);
+
+const API_KEY =YOUR_API_KEY
+
+const mainEle = document.getElementById('main');
+const formEle =document.getElementById('form');
+const searchEle = document.getElementById('search')
+
+
+
 async function getWeatherByLocation(city){
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
-  const weather = await response.json()
-  console.log("weather",weather)
-//   getWeatherByDay(woeid)
+  const data = await response.json()
+  console.log('data',data)
+  addWeatherDetails(data)
+
 }
 
-// async function getWeatherByDay(id){
-//     const response = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${id}`)
-//     const data = await response.json();
-//     console.log("data",data)
-// }
+function addWeatherDetails(data){
+  const weather  = document.createElement('div')
+  weather.classList.add('weather')
+  weather.innerHTML=`
+  <h2>${data.main.temp}°C</h2>
+  <p>${data.name}</p>
+  `
+  //  clean up the element
+  mainEle.innerHTML=""
+  //  append item to element
+  mainEle.appendChild(weather)
+}
 
-getWeatherByLocation('london')
+function searchWeather(e){
+   e.preventDefault();
+   const city = searchEle.value; 
+   if(city){
+     getWeatherByLocation(city)
+   }
+}
+
+formEle.addEventListener('submit',searchWeather)
+
